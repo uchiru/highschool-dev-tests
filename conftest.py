4762c4b95eac9a3594afbf9c956c1be21f4d1722
Saharock1814 <verden.rinkihika@gmail.com>
2022-04-06 16:38:54 +0300

    Обновление конфтест для си ай, добавление реквайрментс

--- conftest.py
+++ conftest.py
@@ -2,18 +2,43 @@ import os
 import pytest
 from selenium import webdriver
 from selenium.webdriver.remote import remote_connection
-from pageobject.methods import *
+from pageobjects.methods import *
 from constance import *
+from selenium.webdriver.chrome.options import Options
 import time
+from selenium.webdriver.remote import remote_connection
+
+
+def pytest_addoption(parser):
+    """Create options from console for tests env"""
+    parser.addoption('--bn', action='store', default="chrome", help="Choose browser: chrome, remote_chrome or firefox")
+    parser.addoption('--h', action='store', default="enable", help='Choose headless: enable or other')
+    parser.addoption('--s', action='store', default="1920,1080", help='Size window: width,height')
+
+@pytest.fixture
+def browser(request):
+    if request.config.getoption("bn") == "remote_chrome":
+        capabilities = {
+            "browserName": "chrome",
+            "enableVNC": False,
+            "enableVideo": False }
+        url = os.environ['SELENOID_URL']
+        conn = webdriver.remote.remote_connection.RemoteConnection(url, resolve_ip=False)
+        print("\nstart browser for test..")
+        browser = webdriver.Remote(command_executor=conn, desired_capabilities=capabilities)
+        yield browser
+        #этот код выполнится после завершения теста
+        print("\nquit browser..")
+        browser.quit()
 
 
 @pytest.fixture(scope='class')
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome('/Users/sah_m/Desktop/Foravtotests/chromedriver 4')
-    browser.maximize_window()
+def browser(request):
+    # return request.config.getoption("--language")
+    options = Options()
+    browser = webdriver.Chrome(options=options)
+    print("\nstart chrome browser for test..")
     yield browser
-    print("\nquit browser..")
     browser.quit()
 
 
@@ -36,4 +61,15 @@ def avtorithaision_b2c(browser, login_b2c='29', password_b2c='32407собор'):
     time.sleep(3)
     password_input = wdw(browser, 35).until(
         EC.presence_of_element_located(avtor_page_elements.password_input)).send_keys(password_b2c)
+    enter = wdw(browser, 15).until(EC.presence_of_element_located(avtor_page_elements.enter_button)).click()
+
+
+@pytest.fixture(scope='class')
+def avtorithaision_techear(browser, login_teacher='teacher124@uchi.ru', password_teacher='1'):
+    browser.get('https://57211.shot-uchi.ru/')
+    login_input = wdw(browser, 15).until(EC.presence_of_element_located(avtor_page_elements.login_input)).send_keys(
+       login_teacher)
+    time.sleep(3)
+    password_input = wdw(browser, 35).until(
+        EC.presence_of_element_located(avtor_page_elements.password_input)).send_keys(password_teacher)
     enter = wdw(browser, 15).until(EC.presence_of_element_located(avtor_page_elements.enter_button)).click()
--- constance.py
+++ constance.py
@@ -1,5 +1,5 @@
 
-
+teach_subjects = ['bio', 'lit'] # список чекбоксов выбираемых на странице предметов преподаваемых учителем
 new_subjects = ['bio', 'lit', 'biolit'] # список чекбоксов выбираемых на странице тарифов
 old_subjects = ['math', 'rus', 'eng', 'prog', 'plan', 'all_check']
 old_subjects_not_year = ['math', 'rus', 'eng', 'prog', 'all_check']
@@ -10,14 +10,6 @@ paypass_1 = '4'
 
 
 
-class login_constance():
-
-    login_admin = 'admin+admin@uchi.ru'
-    password_admin = '1'
-    login_b2t = '141'
-    password_b2t = '19468персик'
-    login_b2c = '29'
-    password_b2c = '32407собор'
 
 
 class card_data():
