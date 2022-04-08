8f50caf2a46f102ccf9a436a73c6ea2b3757a68a
Saharock1814 <verden.rinkihika@gmail.com>
2022-04-08 15:40:55 +0300

    подтянул тайминги

--- pageobject/methods.py
+++ pageobject/methods.py
@@ -191,20 +191,20 @@ def old_subjects_halfyear(browser, card_1, mmyy_1, cvv_1, paypass_1, choose_chec
 def new_subjects_month(browser, card_1, mmyy_1, cvv_1, paypass_1, choose_checkbox):
     browser.get('https://57211.shot-uchi.ru/profile/students')
     full_version = wdw(browser, 30).until(EC.presence_of_element_located(headpage_elements.full_version_button)).click()
-    gradusnick_click = wdw(browser, 15).until(
+    gradusnick_click = wdw(browser, 25).until(
         EC.presence_of_element_located(payments_page_elements.gradusnik_month)).click()
     if choose_checkbox == 'bio':
-        half_year_click = wdw(browser, 15).until(
+        half_year_click = wdw(browser, 25).until(
             EC.presence_of_element_located(payments_page_elements.gradusnik_halfyear)).click
         modern_biology_click = wdw(browser, 30).until(
             EC.presence_of_element_located(payments_page_elements.modern_biology_check_box)).click()
     elif choose_checkbox == 'lit':
-        half_year_click = wdw(browser, 15).until(
+        half_year_click = wdw(browser, 25).until(
             EC.presence_of_element_located(payments_page_elements.gradusnik_halfyear)).click
         literature_click = wdw(browser, 30).until(
             EC.presence_of_element_located(payments_page_elements.modern_literature_check_box)).click()
     else:
-        half_year_click = wdw(browser, 15).until(
+        half_year_click = wdw(browser, 25).until(
             EC.presence_of_element_located(payments_page_elements.gradusnik_halfyear)).click
         modern_biology_click = wdw(browser, 30).until(
             EC.presence_of_element_located(payments_page_elements.modern_biology_check_box)).click()
@@ -212,9 +212,9 @@ def new_subjects_month(browser, card_1, mmyy_1, cvv_1, paypass_1, choose_checkbo
             EC.presence_of_element_located(payments_page_elements.modern_literature_check_box)).click()
     skip_over_checkbox(browser)
     buy_click = wdw(browser, 25).until(EC.presence_of_element_located(payments_page_elements.buy_button)).click()
-    card = wdw(browser, 15).until(EC.presence_of_element_located(card_data_page_elements.card_num)).send_keys(
+    card = wdw(browser, 25).until(EC.presence_of_element_located(card_data_page_elements.card_num)).send_keys(
         card_data.card_1)
-    mmyy = wdw(browser, 15).until(EC.presence_of_element_located(card_data_page_elements.card_date)).send_keys(
+    mmyy = wdw(browser, 25).until(EC.presence_of_element_located(card_data_page_elements.card_date)).send_keys(
         card_data.mmyy_1)
     cvv = wdw(browser, 25).until(EC.presence_of_element_located(card_data_page_elements.cvv_num)).send_keys(
         card_data.cvv_1)
@@ -229,7 +229,7 @@ def new_subjects_month(browser, card_1, mmyy_1, cvv_1, paypass_1, choose_checkbo
 def old_subjects_month(browser, card_1, mmyy_1, cvv_1, paypass_1, choose_check_box):
     browser.get('https://57211.shot-uchi.ru/profile/students')
     full_version = wdw(browser, 30).until(EC.presence_of_element_located(headpage_elements.full_version_button)).click()
-    gradusnick_click = wdw(browser, 15).until(
+    gradusnick_click = wdw(browser, 25).until(
         EC.presence_of_element_located(payments_page_elements.gradusnik_month)).click()
     if choose_check_box == 'math':
         rus_click = wdw(browser, 20).until(
@@ -247,7 +247,7 @@ def old_subjects_month(browser, card_1, mmyy_1, cvv_1, paypass_1, choose_check_b
         rus_click = wdw(browser, 20).until(
             EC.presence_of_element_located(payments_page_elements.rus_check_box)).click()
     elif choose_check_box == 'prog':
-        prog_click = wdw(browser, 10).until(
+        prog_click = wdw(browser, 20).until(
             (EC.presence_of_element_located(payments_page_elements.prog_check_box))).click()
         math_click = wdw(browser, 20).until(
             EC.presence_of_element_located(payments_page_elements.math_check_box)).click()
