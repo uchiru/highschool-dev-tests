d9a048765b4b7f502c2baddad490d60e187f3033
Saharock1814 <verden.rinkihika@gmail.com>
2022-04-19 23:21:19 +0300

    Добавил учительский менеджмент предметов

---
+++ test_suit_teacher.py
@@ -0,0 +1,19 @@
+from pageobject.methods import *
+import pytest
+from constance import *
+from selenium.webdriver.support.ui import WebDriverWait as wdw
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.mark.usefixtures('browser', 'avtorithaision_techear')
+class Test_teacher_sub_management:
+
+    @pytest.mark.parametrize('checkboxes', teach_subjects)
+    def test_sub_management(self, browser, checkboxes):
+        teach_sub_management(browser, checkboxes)
+        if checkboxes == 'bio':
+            assert len(browser.find_elements_by_css_selector('[data-qa-marker="menu-modern_biology"]')) == 0, 'biology missing at progrrams list'
+        elif checkboxes == 'lit':
+            assert len(browser.find_elements_by_css_selector('[data-qa-marker="menu-modern_literature"]')) == 0, 'literature missing at programs list'
+        else:
+            assert len(browser.find_elements_by_css_selector('[data-qa-marker="menu-modern_biology"]')) == 1 and len(browser.find_elements_by_css_selector('[data-qa-marker="menu-modern_literature"]')) == 1, 'biology or literature missing at progrrams list'
