9cc42c2e7c0356d6cbdb1445fb4ec6359f2e499e
Saharock1814 <verden.rinkihika@gmail.com>
2022-05-11 13:45:51 +0300

    Добавил тайм слипы для покупок

--- test_suit_b2t.py
+++ test_suit_b2t.py
@@ -41,6 +41,7 @@ class Test_high_school_buys_b2t:
         assert current_url == congrat, 'not correct page'
         congrat_button_click = wdw(browser, 10).until(
             EC.presence_of_element_located(congrat_page_elements.congrat_button)).click()
+        time.sleep(5)
 
     @pytest.mark.parametrize('old_checkboxes', old_subjects)
     def test_old_subjects_buy_year(self, browser, old_checkboxes):
@@ -50,6 +51,7 @@ class Test_high_school_buys_b2t:
         assert current_url == congrat, 'not correct page'
         congrat_button_click = wdw(browser, 10).until(
             EC.presence_of_element_located(congrat_page_elements.congrat_button)).click()
+        time.sleep(5)
 
     @pytest.mark.new_code_try
     @pytest.mark.parametrize('checkboxes', new_subjects)
@@ -60,6 +62,7 @@ class Test_high_school_buys_b2t:
         assert current_url == congrat, 'not correct page'
         congrat_button_click = wdw(browser, 10).until(
             EC.presence_of_element_located(congrat_page_elements.congrat_button)).click()
+        time.sleep(5)
 
     @pytest.mark.parametrize('old_checkboxes', old_subjects_not_year)
     def test_old_subjects_buy_half_year(self, browser, old_checkboxes):
@@ -69,6 +72,7 @@ class Test_high_school_buys_b2t:
         assert current_url == congrat, 'not correct page'
         congrat_button_click = wdw(browser, 10).until(
             EC.presence_of_element_located(congrat_page_elements.congrat_button)).click()
+        time.sleep(5)
 
     @pytest.mark.parametrize('checkboxes', new_subjects)
     def test_modern_subjects_buy_month(self, browser, checkboxes):
@@ -78,6 +82,7 @@ class Test_high_school_buys_b2t:
         assert current_url == congrat, 'not correct page'
         congrat_button_click = wdw(browser, 10).until(
             EC.presence_of_element_located(congrat_page_elements.congrat_button)).click()
+        time.sleep(5)
 
     @pytest.mark.parametrize('old_checkboxes', old_subjects_not_year)
     def test_old_subjects_buy_month(self, browser, old_checkboxes):
@@ -87,3 +92,4 @@ class Test_high_school_buys_b2t:
         assert current_url == congrat, 'not correct page'
         congrat_button_click = wdw(browser, 10).until(
             EC.presence_of_element_located(congrat_page_elements.congrat_button)).click()
+        time.sleep(5)
